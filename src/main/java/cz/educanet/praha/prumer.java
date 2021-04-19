59043d0ed3e450de61c34e030a0bdd137ee19ef6
Dave555lol <58588215+Dave555lol@users.noreply.github.com>
2021-04-19 13:44:18 +0200

    ‎

---
+++ src/main/java/cz/educanet/praha/prumer.java
@@ -0,0 +1,24 @@
+package cz.educanet.praha;
+
+import java.util.*;
+
+public class prumer {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Prumer je " + avg(sc));
+    }
+
+    public static double avg(Scanner scnr) {
+        System.out.print("Kolik cisel: ");
+        int numbers = scnr.nextInt();
+        double s = 0;
+        for (int i = 0; i < numbers; i++) {
+            System.out.print("Zadej " + (i + 1) + ". cislo: ");
+            s = s + scnr.nextDouble();
+        }
+        double result = s / numbers;
+        return result;
+    }
+}
