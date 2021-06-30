918044ec2343f36d6ef0df91ac86819096009db8
Dave555lol <58588215+Dave555lol@users.noreply.github.com>
2021-06-30 23:40:54 +0200

    Update MostAverageNumber.java

--- src/main/java/cz/educanet/praha/MostAverageNumber.java
+++ src/main/java/cz/educanet/praha/MostAverageNumber.java
@@ -4,39 +4,23 @@ public class MostAverageNumber {
 
     public static void main(String[] args) {
 
-        String[] pole = new String[]{"Zdravime ze Zbeslavi","Dobry den", "Jak se mas?", "Ja jsem John nebo Pepa", "Dnes je ctvrtek", "Zitra je patek", "A potom sobota"};
+        double z[] ={1,5,7,6};
 
-        System.out.println(findStringWithMostOccurrencesOfLetter(pole,'e'));
+		double u = 0;
+    	for (int i = 0; i < z.length;i++){
+    		u = u + z[i];
+    	}
+    	double avg = u/ z.length;
 
+			double mindelta = 0;
+			double result = 0;
+    	for(int i = 0;i<z.length;i++){
+    		double delta = Math.abs(avg-z[i]);
+    		System.out.println(delta);
 
-    }
-
-
-    public static String findStringWithMostOccurrencesOfLetter(String[] strings, char letter) {
-
-        int[] pocetPismen = new int[strings.length];
-
-        for (int i = 0;i < strings.length;i++) {
-            String veta = strings[i];
-
-            for (int k = 0; k < veta.length(); k++) {
-                if (veta.charAt(k) == letter) {
-                    pocetPismen[i]++;
-                }
-            }
-        }
-
-        int nejvetsiCislo = pocetPismen[0];
-        int index = 0;
-
-        for (int z = 1;z < pocetPismen.length; z++){
-            if (pocetPismen[z] > nejvetsiCislo){
-                nejvetsiCislo = pocetPismen[z];
-                index = z;
-            }
-        }
-
-        return strings[index];
-
-    }
-}
+    		if (delta>mindelta){
+    			result = z[i];
+    			mindelta = delta;
+			}
+		}
+    	System.out.println(result);
